fe1ebbd628004185004b8b3a7184e62779b76429
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-03-02 11:44:59 +0100

    delete

--- index.js
+++ index.js
@@ -7,16 +7,7 @@ const joi = require("joi");
 
 server.use(express.json());
 
-/*
-Man ska kunna hämta information om ett land.
-Informationen måste vara följande: Befolkning, huvudstad och språk.
-En klient med en säkerhetskod ska kunna lägga till nya länder.
-En klient med en säkerhetskod ska kunna redigera redan existerande länder.
-En klient med en säkerhetskod ska kunna ta bort redan existerande länder.
-Ni måste använda minst en route.
-Ni måste ha validering för varje endpoint (Går bra att täcka flera endpoints med en middleware).
-Era endpoints måste svara med relevanta statuskoder och meddelanden.
-*/
+
 
 const config = {
   user: process.env.DB_USER,
@@ -32,6 +23,67 @@ server.get("/", (req, res) => {
 
   const {country} = req.body;
   
+  const exists = `
+  SELECT EXISTS(SELECT * FROM countryData WHERE Country = ?)`;
+
+  pool.query(exists, [country], (error, result) => {
+
+    if (error) {
+      res.sendStatus(500);
+    } else {
+
+    if(Object.values(result[0])[0] === 0) {
+
+    console.log("nej");
+    res.status(404).json('Country does not exist in database');
+    return;
+    }
+    
+    const sql = `
+    SELECT * FROM countryData WHERE Country = ?`
+  
+    pool.query(sql, [country], (error, result) => {
+      
+      if (error) {
+        console.log(error);
+        res.sendStatus(500);
+      } else {
+        res.status(200).send(result);
+      }
+  })
+    }
+  })
+});
+
+
+server.post("/addCountry", (req, res) => {
+  const {password} = req.body;
+
+  // Country, Capital, Population, mainLanguage
+  const sql = `INSERT INTO countryData VALUES ('Norge', 'Oslo', 20000000, 'Norska')`;
+  
+  const schema = joi.object({
+    password: joi.string().min(8).max(25).required()
+});
+
+const validation = schema.validate(req.body);
+
+});
+/*
+Man ska kunna hämta information om ett land.
+Informationen måste vara följande: Befolkning, huvudstad och språk.
+En klient med en säkerhetskod ska kunna lägga till nya länder.
+En klient med en säkerhetskod ska kunna redigera redan existerande länder.
+En klient med en säkerhetskod ska kunna ta bort redan existerande länder.
+Ni måste använda minst en route.
+Ni måste ha validering för varje endpoint (Går bra att täcka flera endpoints med en middleware).
+Era endpoints måste svara med relevanta statuskoder och meddelanden.
+*/
+
+
+server.put('/edit', (req, res) => {
+  const {country, capital, population, mainLanguage} = req.body;
+
   const exists = `
   SELECT EXISTS(SELECT * FROM countryData WHERE Country = '${country}')`;
 
@@ -49,24 +101,55 @@ server.get("/", (req, res) => {
     }
     
     const sql = `
-    SELECT * FROM countryData WHERE Country = '${country}'`
+    DELETE FROM countryData WHERE Country = '${country}'`
   
     pool.query(sql, (error, result) => {
+      if (error) {
+        console.log(error);
+        res.sendStatus(500);
+      } else {
+        res.status(200).send(`${country} was edited!`);
+      }
+  })
+    }
+  })
+})
+
+
+
+server.delete('/delete', (req, res) => {
+  const {country} = req.body;
+
+  const exists = `
+  SELECT EXISTS(SELECT * FROM countryData WHERE Country = ?)`;
+
+  pool.query(exists, [country], (error, result) => {
+
+    if (error) {
+      res.sendStatus(500);
+    } else {
+
+    if(Object.values(result[0])[0] === 0) {
+
+    console.log("nej");
+    res.status(404).json('Country does not exist in database');
+    return;
+    }
+    
+    const sql = `
+    DELETE FROM countryData WHERE Country = ?`
+  
+    pool.query(sql, [country], (error, result) => {
       
       if (error) {
         console.log(error);
         res.sendStatus(500);
       } else {
-        res.status(200).send(result);
+        res.status(200).send(`${country} was deleted from the database!`);
       }
   })
     }
   })
-});
-
-
-server.post("/addCountry", (req, res) => {
-  
-});
+})
 
 server.listen(5050);
