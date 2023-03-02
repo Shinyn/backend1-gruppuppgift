ba11f9f568b21f90d0b5775ade5d2855d2245d05
Shinyn <mattias.alm@hotmail.com>
2023-03-02 09:29:17 +0100

    get

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const server = express();
+const { country } = require("./server");
 
 server.get("/", (req, res) => {
-  console.log("hello");
+  console.log("hello", country);
+  res.status(200).send(country);
 });
 
 server.listen(5050);
